0764d61bb63f033dddddfbb3447a0c0de15489b8
Clive-04 <202310384@fit.edu.ph>
2025-05-16 13:40:09 +0800

    feat: added footer component

---
+++ components/footer.component.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark text-light py-4 mt-auto">
+    <div class="container text-center">
+        <p class="mb-1">&copy; <?= date('Y') ?> Cat Breed Directory. All rights reserved.</p>
+        <p class="mb-0">
+            <a href="/index.php" class="text-light">Home</a> |
+            <a href="/page/catBreeds/index.php" class="text-light">Cat Breeds</a>
+        </p>
+    </div>
+</footer>
