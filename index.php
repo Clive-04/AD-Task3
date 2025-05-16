95364bf2e3fb9ee4ec58fa7599abcd7da62f2bc6
Clive-04 <202310384@fit.edu.ph>
2025-05-16 11:47:50 +0800

    feat: added a home page on the index root

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include BASE_PATH . '/components/header.component.php';
+?>
+
+<main class="home-main">
+    <section class="intro">
+        <h2>Welcome to Cat Breed Directory!</h2>
+        <p>Learn about different cat breeds around the world. Browse details like origin and lifespan.</p>
+        <a href="/page/catBreeds/index.php" class="btn">View Cat Breeds</a>
+    </section>
+</main>
+
+</body>
+</html>
