29b03ad79bf418a3045de48e92aa9f3e1ce2971b
Clive-04 <202310384@fit.edu.ph>
2025-05-16 11:29:50 +0800

    feat: added an array in util

---
+++ utils/CatData.util.php
@@ -0,0 +1,22 @@
+<?php
+    function getAllCatBreed(): array {
+        return [
+            "Persian" => [
+                "origin" => "Iran",
+                "lifespan" => "12-17 years",
+                "image" => "/assets/img/persian.png"
+            ],
+
+            "Siamese" => [
+                "origin" => "Thailand",
+                "lifespan" => "10-15 years",
+                "image" => "page/catBreeds/assets/img/siamese.png"
+            ],
+
+            "Maine Coon" => [
+            "origin" => "USA",
+            "lifespan" => "10-13 years",
+            "image" => ""
+            ]
+        ];
+    }
