8658df1261af24fa8f59530b947ee30e1bc749fe
Clive-04 <202310384@fit.edu.ph>
2025-05-16 12:12:30 +0800

    feat: added bootstrap on index.php

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
     require_once 'bootstrap.php';
 ?>
 
-<main class="home-main">
-    <section class="intro">
-        <h2>Welcome to Cat Breed Directory!</h2>
-        <p>Learn about different cat breeds around the world. Browse details like origin and lifespan.</p>
-        <a href="/page/catBreeds/index.php" class="btn">View Cat Breeds</a>
+<main class="container">
+    <section class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h2 class="display-5 fw-bold">Welcome to Cat Breed Directory!</h2>
+            <p class="col-md-8 fs-4">Learn about different cat breeds around the world. Browse details like origin and lifespan.</p>
+            <a href="/page/catBreeds/index.php" class="btn btn-primary btn-lg">View Cat Breeds</a>
+        </div>
     </section>
 </main>
 
