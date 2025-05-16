6d5cc717764d849944e9e2fe325d9a827f92c0c0
Clive-04 <202310384@fit.edu.ph>
2025-05-16 12:41:00 +0800

    feat: added more cats inside util

--- utils/CatData.util.php
+++ utils/CatData.util.php
@@ -14,9 +14,45 @@
             ],
 
             "Maine Coon" => [
-            "origin" => "USA",
-            "lifespan" => "10-13 years",
-            "image" => "/assets/img/mainecoon.jpg"
+                "origin" => "USA",
+                "lifespan" => "10-13 years",
+                "image" => "/assets/img/mainecoon.jpg"
+            ],
+
+            "Ragdoll" => [
+                "origin" => "USA",
+                "lifespan" => "12-17 years",
+                "image" => ""
+            ],
+
+            "Bengal" => [
+                "origin" => "USA",
+                "lifespan" => "12-16 years",
+                "image" => ""
+            ],
+
+            "Sphynx" => [
+                "origin" = "Canada",
+                "lifespan" => "8-14 years",
+                "image" => ""
+            ],
+
+            "Scottish Fold" => [
+                "origin" => "Scotland",
+                "lifespan" => "11-15 years",
+                "image" => ""
+            ]
+
+            "British Shorthair" => [
+                "origin" => "United Kingdom",
+                "lifespan" => "12-20 years",
+                "image"=> ""
+            ]
+
+            "Japanese Bobtail" => [
+                "origin" = "Japan",
+                "lifespan" => "9-15 years",
+                "image" => ""
             ]
         ];
     }
