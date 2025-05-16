902f09dcf4a6194491221a795c0cc6a671d833b9
Clive-04 <202310384@fit.edu.ph>
2025-05-16 12:20:37 +0800

    feat: added bootstrap on catBreeds index

--- page/catBreeds/index.php
+++ page/catBreeds/index.php
@@ -1,12 +1,22 @@
 <?php
-    require_once '../../utils/CatData.util.php';
-    require_once '../../components/templates/catcard.component.php';
-    include '../../components/header.component.php';
+    require_once '../../bootstrap.php';
 
-    $breeds = getAllCatBreeds();
+    $cats = getAllCatBreeds();
+?>
 
-    echo "<main>";
-    foreach ($breeds as $breed => $details) {
-        renderCatCard($breed, $details);
-    }
-    echo "</main>";
+<main class="container">
+    <div class="row">
+        <?php foreach ($cats as $name => $cat): ?>
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    <img src="<?= $cat['image'] ?>" class="card-img-top" alt="<?= $name ?>">
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><?= $name ?></h5>
+                        <p class="card-text">Origin: <?= $cat['origin'] ?></p>
+                        <p class="card-text">Lifespan: <?= $cat['lifespan'] ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</main>
