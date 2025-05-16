c33d6780876a9071f024c255aea1fe88f5036802
Clive-04 <202310384@fit.edu.ph>
2025-05-16 11:59:11 +0800

    fix: modified the header component

--- components/header.component.php
+++ components/header.component.php
@@ -1,7 +1,15 @@
-<header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Breed Directory</title>
+</head>
+<body>
     <h1>Cat Breed Directory</h1>
     <nav>
-        <a href="/">Home</a> |
+        <a href="/../../index.php">Home</a>
         <a href="/page/catBreeds/index.php">Breeds</a>
     </nav>
-</header>
+</body>
+</html>
